1ec8c6b48437645985316425f5584573b3829b85
tejamounikadevi <tejamounikadevi@gmail.com>
2023-12-19 12:52:32 +0530

    roboshop shell scripts

--- catalogue.sh
+++ catalogue.sh
@@ -49,7 +49,7 @@ mkdir /app
 
 VALIDATE $? "creating app directory" 
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
 
 VALIDATE $? "Downloading catalogue application" 
 
