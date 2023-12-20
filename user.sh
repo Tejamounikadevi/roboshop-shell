97318f526416121dd42d6baf013005d6a7de04d2
tejamounikadevi <tejamounikadevi@gmail.com>
2023-12-21 00:35:38 +0530

    roboshop shell scripts

--- user.sh
+++ user.sh
@@ -93,6 +93,6 @@ dnf install mongodb-org-shell -y &>> $LOGFILE
 
 VALIDATE $? "Installing mongodb client"
 
-mongo --host $MONGODB_HOST </app/schema/user.js &>> $LOGFILE
+mongo --host $MONGODB_HOST /app/schema/user.js &>> $LOGFILE
 
 VALIDATE $? "Loading user data into MONGODB"
