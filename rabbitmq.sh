0c7c19ef3f428756509ab59ca5275f097715a322
tejamounikadevi <tejamounikadevi@gmail.com>
2023-12-22 01:08:27 +0530

    roboshop shell scripts

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -50,13 +50,13 @@ systemctl start rabbitmq-server  &>> $LOGFILE
 
 VALIDATE $? "Starting rabbitmq server"
 
-id roboshop #if roboshop user does not exit, then it is failure
+rabbitmqctl roboshop roboshop123 #if roboshop user does not exit, then it is failure
 if [ $? -ne 0 ]
 then
     rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
     VALIDATE $? "creating user" 
 else
-    echo -e "roboshop user already exist $Y SKIPPING $N"
+    echo -e "roboshop roboshop123 user already exist $Y SKIPPING $N"
 fi
 
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGFILE
