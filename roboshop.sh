5f180b5d6f340b785d070a9c61174150637577de
tejamounikadevi <tejamounikadevi@gmail.com>
2023-12-26 00:54:10 +0530

    daws-76s

--- roboshop.sh
+++ roboshop.sh
@@ -14,5 +14,5 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-0012a7f2b2a6ba551
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-0012a7f2b2a6ba551 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
